b12df8c3c5a2c93ce5eb0c11e7b5ba7e356b2548
Ivan-GIS <ikuznecov@gis.ie>
2025-01-27 11:33:28 +0200

    bootstrap initial code

---
+++ src/main/java/dev/dataivan/mqttdata/MqttdataApplication.java
@@ -0,0 +1,13 @@
+package dev.dataivan.mqttdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dataivan/mqttdata/MqttdataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dataivan.mqttdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
